045c4309a8a016880b474098047b28d81e976dfa
Johanna Selander <johanna.selander.JAVA23@edu.edugrade.se>
2023-12-12 13:09:51 +0100

    Skapat metod som kan appliceras för att ladda alla fxml filer

--- src/main/java/org/teambravo/pipergames/view/ViewAllController.java
+++ src/main/java/org/teambravo/pipergames/view/ViewAllController.java
@@ -1,22 +1,60 @@
 package org.teambravo.pipergames.view;
 
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Tab;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.Node;
+import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
 public class ViewAllController {
 
     @FXML
+    private Button playerTabButton;
+    @FXML
+    private Button teamTabButton;
+    /*@FXML
     private Tab playerTab;
     @FXML
     private Tab teamTab;
+*/
+
+    /*@FXML
+    private void handlePlayerTabButton(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = loadPlayerTab(event);
+        PlayerTabController playerTabController = fxmlLoader.getController();
+    }
+
+    private FXMLLoader loadPlayerTab(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ViewAllController.class.getResource("/org/teambravo/pipergames/playerTab.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setResizable(false);
+        return fxmlLoader;
+    }*/
 
     @FXML
+    private void handlePlayerTabButton(ActionEvent event) throws IOException {
+        loadFXML("/org/teambravo/pipergames/playerTab.fxml", event, "Spelare");
+    }
+    @FXML
+    private void handleTeamTabButton(ActionEvent event) throws IOException {
+        loadFXML("/org/teambravo/pipergames/teamTab.fxml", event, "Lag");
+    }
+    private void loadFXML(String fxmlPath, ActionEvent event, String title) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlPath));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle(title);
+        stage.setResizable(false);
+    }
+    /*@FXML
     private void handlePlayerTab(Event event) {
         loadTabContent(playerTab, "playerTab.fxml");
     }
@@ -24,9 +62,9 @@ public class ViewAllController {
     @FXML
     private void handleTeamTab(Event event) {
         loadTabContent(teamTab, "teamTab.fxml");
-    }
+    }*/
 
-    private void loadTabContent(Tab tab, String fxmlFile) {
+    /*private void loadTabContent(Tab tab, String fxmlFile) {
         if (tab.getContent() == null) {
             try {
                 Node content = FXMLLoader.load(getClass().getResource(fxmlFile));
@@ -35,7 +73,7 @@ public class ViewAllController {
                 e.printStackTrace();
             }
         }
-    }
+    }*/
 
 
 }
