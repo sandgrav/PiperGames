2b1fb555fee132f26ee64cb00e9b30cea0931bd8
Johanna Selander <johanna.selander.JAVA23@edu.edugrade.se>
2023-12-15 11:45:36 +0100

    Lagt till massa data, bara solo matcherna som inte syns. Även ändrat till rätt java version i alla fxml filer

--- src/main/resources/META-INF/data.sql
+++ src/main/resources/META-INF/data.sql
@@ -1,52 +1,154 @@
--- Insert of data in staff and persons
+-- Skapa personer
 insert into persons(person_first_name, person_last_name, person_nickname) values('Fabian', 'Knopf', 'Fabian');
-SELECT @PersonKey := @@IDENTITY;
-INSERT INTO staff(person_person_id) VALUES (@PersonKey);
+SELECT @PersonKey1 := @@IDENTITY;
 
 insert into persons(person_first_name, person_last_name, person_nickname) values('Linus', 'Axelsson', 'Linus');
-SELECT @PersonKey := @@IDENTITY;
-INSERT INTO staff(person_person_id) VALUES (@PersonKey);
+SELECT @PersonKey2 := @@IDENTITY;
 
 insert into persons(person_first_name, person_last_name, person_nickname) values('Johanna', 'Selander', 'Johanna');
-SELECT @PersonKey := @@IDENTITY;
-INSERT INTO staff(person_person_id) VALUES (@PersonKey);
+SELECT @PersonKey3 := @@IDENTITY;
 
 insert into persons(person_first_name, person_last_name, person_nickname) values('Morten', 'Sandgrav', 'Morten');
-SELECT @PersonKey := @@IDENTITY;
-INSERT INTO staff(person_person_id) VALUES (@PersonKey);
+SELECT @PersonKey4 := @@IDENTITY;
 
--- Insert of data in players and persons
-insert into games(game_name) values ('Fotboll');
-select @GameKey := @@IDENTITY;
+insert into persons(person_first_name, person_last_name, person_nickname) values('Robin', 'Andersson', 'OneHandedOnly'); -- player1
+SELECT @PersonKey5 := @@IDENTITY;
 
-insert into teams(team_name, game_id) values('Arsenal', @GameKey);
-SELECT @TeamKey := @@IDENTITY;
+insert into persons(person_first_name, person_last_name, person_nickname) values('Robert', 'Fridström', 'Lem0naZ'); -- player2
+SELECT @PersonKey6 := @@IDENTITY;
+
+insert into persons(person_first_name, person_last_name, person_nickname) values('Daniel', 'Hedström', 'Drome'); -- player 3
+SELECT @PersonKey7 := @@IDENTITY;
+
+insert into persons(person_first_name, person_last_name, person_nickname) values('Andreas', 'Mellin', 'Andruzo'); -- player 4
+SELECT @PersonKey8 := @@IDENTITY;
+
+insert into persons(person_first_name, person_last_name, person_nickname) values('Elsa', 'Lindberg', 'elslin90');
+SELECT @PersonKey9 := @@IDENTITY;
+
+insert into persons(person_first_name, person_last_name, person_nickname) values('Axel', 'Persson', 'axeper49');
+SELECT @PersonKey10 := @@IDENTITY;
+
+insert into persons(person_first_name, person_last_name, person_nickname) values('Freja', 'Bergström', 'freber58');
+SELECT @PersonKey11 := @@IDENTITY;
+
+insert into persons(person_first_name, person_last_name, person_nickname) values('Hugo', 'Larsson', 'huglar61');
+SELECT @PersonKey12 := @@IDENTITY;
+
+insert into persons(person_first_name, person_last_name, person_nickname) values('Linn', 'Svensson', 'linsve41');
+SELECT @PersonKey13 := @@IDENTITY;
+
+insert into persons(person_first_name, person_last_name, person_nickname) values('Emil', 'Nilsson', 'eminil14');
+SELECT @PersonKey14 := @@IDENTITY;
+
+insert into persons(person_first_name, person_last_name, person_nickname) values('Maja', 'Gustafsson', 'majgus93');
+SELECT @PersonKey15 := @@IDENTITY;
+
+insert into persons(person_first_name, person_last_name, person_nickname) values('Erik', 'Ekström', 'erieks87');
+SELECT @PersonKey16 := @@IDENTITY;
+
+insert into persons(person_first_name, person_last_name, person_nickname) values('Klara', 'Andreasson', 'klaand36');
+SELECT @PersonKey17 := @@IDENTITY;
+
+insert into persons(person_first_name, person_last_name, person_nickname) values('Viktor', 'Johansson', 'vikjoh65');
+SELECT @PersonKey18 := @@IDENTITY;
+
+insert into persons(person_first_name, person_last_name, person_nickname) values('Alice', 'Ström', 'alistr28');
+SELECT @PersonKey19 := @@IDENTITY;
+
+insert into persons(person_first_name, person_last_name, person_nickname) values('Noah', 'Blom', 'noablo12');
+SELECT @PersonKey20 := @@IDENTITY;
 
 
+-- Skapa Spel
+insert into games(game_name) values('League of Legends');
+SELECT @GameKey1 := @@IDENTITY;
+
+insert into games(game_name) values('Fortnite');
+SELECT @GameKey2 := @@IDENTITY;
+
+insert into games(game_name) values('StarCraft II');
+SELECT @GameKey3 := @@IDENTITY;
 
 
+-- Skapa Team
+insert into teams(team_name, game_id) values('Cyber Phoenixes', @GameKey1);
+SELECT @TeamKey1 := @@IDENTITY;
 
-insert into persons(person_first_name, person_last_name, person_nickname) values('Robin', 'Andersson', 'OneHandedOnly');
-SELECT @PersonKey := @@IDENTITY;
-INSERT INTO players(person_person_id, team_id) VALUES (@PersonKey, @TeamKey);
+insert into teams(team_name, game_id) values('Neon Dragons', @GameKey1);
+SELECT @TeamKey2 := @@IDENTITY;
+
+insert into teams(team_name, game_id) values('Quantum Vipers', @GameKey2);
+SELECT @TeamKey3 := @@IDENTITY;
+
+insert into teams(team_name, game_id) values('Galactic Gladiators', @GameKey2);
+SELECT @TeamKey4 := @@IDENTITY;
+
+
+-- Insert persons into players
+INSERT INTO players(person_person_id, team_id, game_id) VALUES (@PersonKey5, @TeamKey1, @GameKey1);
 SELECT @PlayerKey1 := @@IDENTITY;
 
-insert into persons(person_first_name, person_last_name, person_nickname) values('Robert', 'Fridström', 'Lem0naZ');
-SELECT @PersonKey := @@IDENTITY;
-INSERT INTO players(person_person_id, team_id) VALUES (@PersonKey, @TeamKey);
-
+INSERT INTO players(person_person_id, team_id, game_id) VALUES (@PersonKey6, @TeamKey1, @GameKey1);
 SELECT @PlayerKey2 := @@IDENTITY;
-insert into teams(team_name, game_id) values('Manchester', @GameKey);
 
-SELECT @TeamKey := @@IDENTITY;
-insert into persons(person_first_name, person_last_name, person_nickname) values('Daniel', 'Hedström', 'Drome');
-SELECT @PersonKey := @@IDENTITY;
-INSERT INTO players(person_person_id, team_id) VALUES (@PersonKey, @TeamKey);
+INSERT INTO players(person_person_id, team_id, game_id) VALUES (@PersonKey7, @TeamKey1, @GameKey1);
 SELECT @PlayerKey3 := @@IDENTITY;
-insert into persons(person_first_name, person_last_name, person_nickname) values('Andreas', 'Mellin', 'Andruzo');
-SELECT @PersonKey := @@IDENTITY;
-INSERT INTO players(person_person_id, team_id) VALUES (@PersonKey, @TeamKey);
+
+INSERT INTO players(person_person_id, team_id, game_id) VALUES (@PersonKey8, @TeamKey2, @GameKey1);
 SELECT @PlayerKey4 := @@IDENTITY;
+
+INSERT INTO players(person_person_id, team_id, game_id) VALUES (@PersonKey9, @TeamKey2, @GameKey1);
+SELECT @PlayerKey5 := @@IDENTITY;
+
+INSERT INTO players(person_person_id, team_id, game_id) VALUES (@PersonKey10, @TeamKey2, @GameKey1);
+SELECT @PlayerKey6 := @@IDENTITY;
+
+INSERT INTO players(person_person_id, team_id, game_id) VALUES (@PersonKey11, @TeamKey3, @GameKey2);
+SELECT @PlayerKey7 := @@IDENTITY;
+
+INSERT INTO players(person_person_id, team_id, game_id) VALUES (@PersonKey12, @TeamKey3, @GameKey2);
+SELECT @PlayerKey8 := @@IDENTITY;
+
+INSERT INTO players(person_person_id, team_id, game_id) VALUES (@PersonKey13, @TeamKey3, @GameKey2);
+SELECT @PlayerKey9 := @@IDENTITY;
+
+INSERT INTO players(person_person_id, team_id, game_id) VALUES (@PersonKey14, @TeamKey4, @GameKey2);
+SELECT @PlayerKey10 := @@IDENTITY;
+
+INSERT INTO players(person_person_id, team_id, game_id) VALUES (@PersonKey15, @TeamKey4, @GameKey2);
+SELECT @PlayerKey11 := @@IDENTITY;
+
+INSERT INTO players(person_person_id, team_id, game_id) VALUES (@PersonKey16, @TeamKey4, @GameKey2);
+SELECT @PlayerKey12 := @@IDENTITY;
+
+INSERT INTO players(person_person_id, game_id) VALUES (@PersonKey17, @GameKey3);
+SELECT @PlayerKey13 := @@IDENTITY;
+
+INSERT INTO players(person_person_id, game_id) VALUES (@PersonKey18, @GameKey3);
+SELECT @PlayerKey14 := @@IDENTITY;
+
+INSERT INTO players(person_person_id, game_id) VALUES (@PersonKey19, @GameKey3);
+SELECT @PlayerKey15 := @@IDENTITY;
+
+INSERT INTO players(person_person_id, game_id) VALUES (@PersonKey20, @GameKey3);
+SELECT @PlayerKey16 := @@IDENTITY;
+
+
+-- Insert persons into staff
+insert into staff(person_person_id) values (@PersonKey1);
+select @StaffKey1 := @@IDENTITY;
+
+insert into staff(person_person_id) values (@PersonKey2);
+select @StaffKey2 := @@IDENTITY;
+
+insert into staff(person_person_id) values (@PersonKey3);
+select @StaffKey3 := @@IDENTITY;
+
+insert into staff(person_person_id) values (@PersonKey4);
+select @StaffKey4 := @@IDENTITY;
+
+
 -- Insert of data in SoloMatches
-insert into solo_matches(date, player1_id, player2_id) values(DATE(20000101),@PlayerKey1,@PlayerKey2);
-insert into solo_matches(date, player1_id, player2_id) values(DATE(20231231),@PlayerKey3,@PlayerKey4);
+insert into solo_matches(date, player1_id, player2_id, game_name) values(DATE(20000101), @PlayerKey17, @PlayerKey18, @GameKey3);
+insert into solo_matches(date, player1_id, player2_id, game_name) values(DATE(20231231), @PlayerKey19, @PlayerKey20, @GameKey3);
