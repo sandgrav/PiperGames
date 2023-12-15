52164f7e9541b827b0df9e516581feb814ef8fb6
Fabio <fabianknopf.umea@gmail.com>
2023-12-15 13:21:28 +0100

    Nu fungerar deletematchbutton och samspelar med databasen. Samt att uppdatera värden i cellerna.

--- src/main/java/org/teambravo/pipergames/view/SoloMatchTabController.java
+++ src/main/java/org/teambravo/pipergames/view/SoloMatchTabController.java
@@ -13,7 +13,9 @@ import javafx.util.converter.IntegerStringConverter;
 import javafx.util.converter.LocalDateStringConverter;
 import org.teambravo.pipergames.controller.MatchSoloController;
 import org.teambravo.pipergames.controller.MatchTeamController;
+import org.teambravo.pipergames.controller.PlayerController;
 import org.teambravo.pipergames.entity.MatchSolo;
+import org.teambravo.pipergames.entity.Person;
 import org.teambravo.pipergames.entity.Player;
 
 import java.net.URL;
@@ -40,7 +42,11 @@ public class SoloMatchTabController implements Initializable {
     @FXML
     private TableColumn<MatchSolo, String> player1TableColumn;
     @FXML
+    private TableColumn<MatchSolo, String> player1IdTableCol;
+    @FXML
     private TableColumn<MatchSolo, String> player2TableCol;
+    @FXML
+    private TableColumn<MatchSolo, String> player2IdTableCol;
     @FXML
     private final MatchSoloController matchSoloController = new MatchSoloController();
     @FXML
@@ -57,6 +63,14 @@ public class SoloMatchTabController implements Initializable {
     private TableColumn<Player, String> nickNameCol;
     @FXML
     private TextField textFieldDeleteMatch;
+    @FXML
+    private TextField player1AddToMatchText;
+
+    @FXML
+    private TextField player2AddToMatchText;
+
+    @FXML
+    private TextField dateAddSoloMatchText;
 
     int enteredMatchId;
 
@@ -70,7 +84,8 @@ public class SoloMatchTabController implements Initializable {
         matchTable.getItems().remove(selectedMatchId);
     } else showAlert("Inget match vald", "Vänligen välj en match att ta bort.");
     }
-
+    @FXML
+    protected void handleAddSoloMatchButton (ActionEvent e){}
 
     @FXML
     protected void handleShowAllMatchesButtonAction (ActionEvent e){
@@ -86,57 +101,57 @@ public class SoloMatchTabController implements Initializable {
         }
     @FXML
     private void handleUpdateMatchButton(ActionEvent event) {
-        matchTable.setEditable(true);
-        dateCol.setCellFactory(TextFieldTableCell.forTableColumn());
-        dateCol.setOnEditCommit(editEvent -> {
-            MatchSolo editedItem = editEvent.getRowValue();
+        MatchSolo selectedMatch = matchTable.getSelectionModel().getSelectedItem();
+        if (selectedMatch != null) {
             try {
+                // Uppdatera datum
+                LocalDate newDate = LocalDate.parse(dateAddSoloMatchText.getText(), DateTimeFormatter.ISO_LOCAL_DATE);
+                selectedMatch.setDate(newDate.atStartOfDay());
 
-                LocalDate newDate = LocalDate.parse(editEvent.getNewValue(), DateTimeFormatter.ISO_LOCAL_DATE);
-                editedItem.setDate(newDate.atStartOfDay());
+                // Uppdatera spelare 1
+                Player player1 = selectedMatch.getPlayer1();
+                player1.getPerson().setNickName(player1AddToMatchText.getText());
+
+                // Uppdatera spelare 2
+                Player player2 = selectedMatch.getPlayer2();
+                player2.getPerson().setNickName(player2AddToMatchText.getText());
 
                 // Uppdatera databasen
-                matchSoloController.updateMatchSoloPlayer(editedItem);
+                matchSoloController.updateMatchSoloPlayer(selectedMatch);
 
-                showAlert("Match Uppdaterad", "Datum uppdaterat!");
+                showAlert("Match Uppdaterad", "Matchen har uppdaterats!");
             } catch (DateTimeParseException e) {
                 showAlert("Fel", "Ogiltigt datumformat. Använd formatet YYYY-MM-DD");
-            }
-        });
-        player1TableColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-        player1TableColumn.setOnEditCommit(editEvent -> {
-            Player editedItem = editEvent.getRowValue().getPlayer1();
-            try {
-                String newPlayer1Value = editEvent.getNewValue();
-                // Uppdatera spelaren i databasen
-                editedItem.getPerson().setNickName(newPlayer1Value);
-
-                // Uppdatera databasen
-                matchSoloController.updateMatchSoloPlayer(matchTable.getSelectionModel().getSelectedItem());
-
-                showAlert("Match Uppdaterad", "Spelare 1 uppdaterad!");
             } catch (Exception e) {
-                showAlert("Fel", "Ett fel uppstod vid uppdatering av Spelare 1.");
+                showAlert("Fel", "Ett fel uppstod vid uppdatering av matchen.");
             }
-        });
-        player2TableCol.setCellFactory(TextFieldTableCell.forTableColumn());
-        player2TableCol.setOnEditCommit(editEvent -> {
-            Player editedItem = editEvent.getRowValue().getPlayer2();
-            try {
-                String newPlayer2Value = editEvent.getNewValue();
-                // Uppdatera spelaren i databasen eller gör vad som behövs
-                editedItem.getPerson().setNickName(newPlayer2Value);
-
-                // Uppdatera databasen
-                matchSoloController.updateMatchSoloPlayer(matchTable.getSelectionModel().getSelectedItem());
-
-                showAlert("Match Uppdaterad", "Spelare 2 uppdaterad!");
-            } catch (Exception e) {
-                showAlert("Fel", "Ett fel uppstod vid uppdatering av Spelare 2.");
-            }
-        });
-        //Hej
+        } else {
+            showAlert("Inget match vald", "Vänligen välj en match att uppdatera.");
+        }
     }
+
+
+
+        /*protected void handleAddMatchButton(ActionEvent e) {
+            if (!(firstNameText.getText().isEmpty() || lastNameText.getText().isEmpty() || nickNameText.getText().isEmpty())) {
+                Dropdown-Lista: här också.
+
+
+                Player player = new Player();
+                player.setPerson(new Person());
+                PlayerController playerController = new PlayerController();
+                player.getPerson().setFirstName(firstNameText.getText());
+                player.getPerson().setLastName(lastNameText.getText());
+                player.getPerson().setNickName(nickNameText.getText());
+                player.getPerson().setAddress(streetAdressText.getText());
+                player.getPerson().setPostalCode(zipText.getText());
+                player.getPerson().setCity(cityText.getText());
+                player.getPerson().setCountry(countryText.getText());
+                player.getPerson().setEmail(eMailText.getText());
+//            player.getTeam().getName()teamText.setText();
+                playerController.savePlayer(player);
+            }
+        }*/
 
     private void showAlert(String title, String content) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
