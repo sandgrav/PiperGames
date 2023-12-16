9c7acefec8a0605425793ff70aa3ae930e787b92
Morten Sandgrav <morten@sandgrav.se>
2023-12-16 15:46:13 +0100

    Börjat på fönster för turnering.

---
+++ src/main/java/org/teambravo/pipergames/view/TournamentTabController.java
@@ -0,0 +1,48 @@
+package org.teambravo.pipergames.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
+public class TournamentTabController {
+    @FXML
+    private ComboBox spelCmb;
+    @FXML
+    private ComboBox spelLagCmb;
+    @FXML
+    private DatePicker dateQuarterFinal;
+    @FXML
+    private DatePicker dateSemiFinal;
+    @FXML
+    private DatePicker dateFinal;
+    @FXML
+    private Label label1;
+    @FXML
+    private Label label2;
+    @FXML
+    private Label label3;
+    @FXML
+    private Label label4;
+    @FXML
+    private Label label5;
+    @FXML
+    private Label label6;
+    @FXML
+    private Label label7;
+    @FXML
+    private Label label8;
+    @FXML
+    private Label label9;
+    @FXML
+    private Label label10;
+    @FXML
+    private Label label11;
+    @FXML
+    private Label label12;
+    @FXML
+    private Label label14;
+    @FXML
+    private Label label15;
+
+}
